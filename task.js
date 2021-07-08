553a65e3b3e3d4769337e82d44176073240f4656
valentin <valik6426588@gmail.com>
2021-07-08 22:00:30 +0300

    3

--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-
+//аккордион
 document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
     item.addEventListener('click', () => {
         const parent = item.parentNode;
@@ -13,3 +13,40 @@ document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
         }
     })
 )
+//открытие модального окна
+let buttonOrder = document.querySelector("button.header__button");
+let form = document.querySelector('form.form-popup');
+let closeForm = document.querySelector('span.popup-header__close');
+buttonOrder.onclick = function () {
+    form.style.display="flex"
+}
+// закрытие модального окна по клику на крестик
+closeForm.onclick = function () {
+    form.style.display = 'none';
+}
+//валидация
+// 1)определяем элементы с HTML
+let name = document.getElementById('popup__text'); 
+let phone = document.getElementById('popup__tel');
+let buttonPopup = document.getElementById('popup__button');
+
+let alertFolseName = document.querySelector('.alertFolseName');
+let alertFolsePhone = document.querySelector('.alertFolsePhone');
+
+jQuery(function ($) {
+    //создаем свою маску для номера телефона
+    $("#number").mask("+375(99) 999-99-99")
+})
+
+buttonPopup.addEventListener('click', checkValid);
+function checkValid(event) {
+    event.preventDefault;
+    if (name.value == '') {      
+        alertFolseName.innerHTML = 'Обязательное поле';
+        
+    } else if (phone.value == '') {
+        alertFolsePhone.innerHTML = 'Обязательное поле';
+    }
+}
+
+console.log (form)
